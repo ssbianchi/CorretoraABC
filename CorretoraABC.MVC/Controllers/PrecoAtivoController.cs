c701e380ca03d36126559a6b84306a9056b0a9dc
Sergio Bianchi <ss.bianchi@gmail.com>
2022-03-31 00:03:29 -0300

    Completo

--- CorretoraABC.MVC/Controllers/PrecoAtivoController.cs
+++ CorretoraABC.MVC/Controllers/PrecoAtivoController.cs
@@ -8,23 +8,72 @@ namespace CorretoraABC.MVC.Controllers
     public class PrecoAtivoController : Controller
     {
         private readonly IPrecoAtivoApp _precoAtivoApp;
+        JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
         public PrecoAtivoController(IPrecoAtivoApp precoAtivoApp)
         {
             _precoAtivoApp = precoAtivoApp;
         }
+        //public async Task<ActionResult> Index()
+        //{
+        //    var result = await _precoAtivoApp.ListarPrecoHistorico();
+        //    var dataPoints = new List<DataPoint>();
+
+        //    foreach (var item in result)
+        //    {
+        //        var date = DateTime.Parse(item.Date).Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+        //        dataPoints.Add(new DataPoint(date, item.ClosePrice));
+        //    }
+        //    ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+        //    return View(result);
+        //}
         public async Task<ActionResult> Index()
         {
-            var result = await _precoAtivoApp.ListarPrecoHistorico();
-            var dataPoints = new List<DataPoint>();
+            try
+            {
+                var result = await _precoAtivoApp.ListarPrecoHistorico();
+                var dataPoints = new List<DataPoint>();
 
+                foreach (var item in result)
+                {
+                    var date = DateTime.Parse(item.Date).Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+                    dataPoints.Add(new DataPoint(date, item.ClosePrice));
+                }
+                //ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+                ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints, _jsonSetting);
+
+                return View(result);
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+        //public async Task<ContentResult> JSON()
+        //{
+        //          var result = await _precoAtivoApp.ListarPrecoHistorico();
+        //          List<DataPoint> dataPoints = new List<DataPoint>();
+        //          foreach (var item in result)
+        //          {
+        //              var date = DateTime.Parse(item.Date).Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+        //              dataPoints.Add(new DataPoint(date, item.ClosePrice));
+        //          }
+
+        //          JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
+        //	return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
+        //}
+        public async Task<ContentResult> JSON()
+        {
+            var result = await _precoAtivoApp.ListarPrecoHistorico();
+            List<DataPointPrecoAtivo> dataPoints = new List<DataPointPrecoAtivo>();
             foreach (var item in result)
             {
                 var date = DateTime.Parse(item.Date).Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
-                dataPoints.Add(new DataPoint(date, item.ClosePrice));
+                dataPoints.Add(new DataPointPrecoAtivo(date, item.OpenPrice, item.HighPrice, item.LowPrice, item.ClosePrice, item.AdjClosePrice, item.Volume));
             }
-            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
 
-            return View(result);
+            JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
+            return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
         }
-	}
+    }
 }
---
+++ CorretoraABC.MVC/Models/DataPointPrecoAtivo.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+
+namespace CorretoraABC.MVC.Models
+{
+    [DataContract]
+    public class DataPointPrecoAtivo
+    {
+        public DataPointPrecoAtivo(double date, decimal openPrice, decimal highPrice, decimal lowPrice, decimal closePrice, decimal adjClosePrice, decimal volume)
+        {
+            this.Date = date;
+            this.OpenPrice = openPrice;
+            this.HighPrice = highPrice;
+            this.LowPrice = lowPrice;
+            this.ClosePrice = closePrice;
+            this.AdjClosePrice = adjClosePrice;
+            this.Volume = volume;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "Date")]
+        public double? Date = null;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "OpenPrice")]
+        public decimal? OpenPrice = null;
+
+        [DataMember(Name = "HighPrice")]
+        public decimal? HighPrice = null;
+
+        [DataMember(Name = "LowPrice")]
+        public decimal? LowPrice = null;
+
+        [DataMember(Name = "ClosePrice")]
+        public decimal? ClosePrice = null;
+
+        [DataMember(Name = "AdjClosePrice")]
+        public decimal? AdjClosePrice = null;
+
+        [DataMember(Name = "Volume")]
+        public decimal? Volume = null;
+    }
+}
